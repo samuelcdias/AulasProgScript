4a8742a14f8b56a56f7b7c2de2e08e55437fe18e
samuelcdias <samuelcardosodf@gmail.com>
2020-11-13 22:08:26 -0300

    add page home

---
+++ frontend/src/components/home/Home.vue
@@ -0,0 +1,64 @@
+<template>
+    <div class="home">
+        <PageTitle 
+            icon="fa fa-home"
+            main="Dashboard"
+            sub="Base de Conhecimento"
+        />
+        <div class="stats">
+            <Stat 
+                title="Categorias" 
+                :value="stat.categories"
+                icon="fa fa-folder"
+                color="#D54D50"
+            />
+            <Stat 
+                title="Artigos" 
+                :value="stat.articles"
+                icon="fa fa-file"
+                color="#3BC480"
+            />
+            <Stat 
+                title="Usuários" 
+                :value="stat.users"
+                icon="fa fa-user"
+                color="#3282CD"
+            />
+        </div>
+    </div>
+</template>
+
+<script>
+
+import PageTitle from '../template/PageTitle'
+import Stat from './Stat'
+import axios from 'axios'
+import {baseApiUrl } from '@/global'
+
+export default {
+    name: 'Home',
+    components: { PageTitle, Stat },
+    data: function(){
+        return {
+            stat: {}
+        }
+    },
+    methods: {
+        getStats(){
+            axios.get(`${baseApiUrl}/stats`).then(res => this.stat = res.data)
+        }
+    },
+    mounted(){
+        this.getStats()
+    }
+}
+</script>
+
+<style>
+    .stats {
+        display: flex;
+        justify-content: space-between;
+        flex-wrap: wrap;
+    }
+
+</style>
--- frontend/src/components/template/Header.vue
+++ frontend/src/components/template/Header.vue
@@ -4,7 +4,7 @@
             <i class="fa fa-lg" :class="icon"> </i>
       </a>
       <h1 class="title">
-          {{ title }}
+          <router-link to="/"> {{ title }}</router-link>
       </h1>
       <UserDropDown v-if="!hideUserDropDown" />
   </header>
